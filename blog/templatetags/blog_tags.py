b99cb66fcf2efe867a50eca0b9f175c7555ba4ea
ent <961522970@qq.com>
2020-07-11 19:45:52 +0800

    评论功能+锚点跳转

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,31 @@
+from ..models import Post,Category,Tag
+from django import template
+
+register = template.Library()
+
+
+@register.inclusion_tag('blog/inclusion/get_recent_posts.html')
+def get_recent_posts(num=5):
+    return {
+        'recent_post_list': Post.objects.all().order_by('-created_time')[:num],
+    }
+@register.inclusion_tag('blog/inclusion/_archives.html')
+def archives():
+    return {
+        'date_list':Post.objects.dates('created_time', 'month', order='DESC')
+    }
+
+
+@register.inclusion_tag('blog/inclusion/_categories.html')
+def get_categories():
+    # 别忘了在顶部引入 Category 类
+    return {
+        'category_list':Category.objects.all(),
+    }
+
+
+@register.inclusion_tag('blog/inclusion/_tags.html')
+def get_tags():
+    return {
+        'tag_list':Tag.objects.all(),
+    }
